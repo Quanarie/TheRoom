f0838c5831362278f84c7a1418d3b9e59decfd56
Quanarie <qustaine@gmail.com>
2022-06-22 13:57:22 +0100

    Added different amount of choices

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -6,12 +6,9 @@ using TMPro;
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] private TextAsset story;
-    [SerializeField] private float speed;
 
     private string[] text;
     private int index;
-
-    private Button[] choices;
 
     private bool isPlayerInRange;
 
@@ -28,15 +25,13 @@ public class Dialogue : MonoBehaviour
     private void Start()
     {
         text = story.text.Split("\n");
+        for (int i = 0; i < text.Length; i++)
+        {
+            print(text[i]);
+        }
         index = 0;
         isPlayerInRange = false;
         dialogueText = DialogueManager.Instance.DialogueText.GetComponent<TextMeshProUGUI>();
-
-        choices = new Button[DialogueManager.Instance.Choices.Length];
-        for (int i = 0; i < choices.Length; i++)
-        {
-            choices[i] = DialogueManager.Instance.Choices[i].GetComponent<Button>();
-        }
     }
 
     private void Update()
@@ -116,6 +111,7 @@ public class Dialogue : MonoBehaviour
     {
         string[] line = readNextLine().Split(";");
         line[0] = line[0].Replace("*", "");
+        Button[] choices = DialogueManager.Instance.ShowChoices(line.Length);
 
         for (int i = 0; i < choices.Length; i++)
         {
@@ -128,15 +124,6 @@ public class Dialogue : MonoBehaviour
         index++; // go to * line so displayChoices() is not called multiple times from update
     }
 
-    private void hideChoices()
-    {
-        foreach(Button choice in choices)
-        {
-            choice.gameObject.SetActive(false);
-            choice.onClick.RemoveAllListeners();
-        }
-    }
-
     private void reactChoice(string choice)
     {
         while (canReadNext() && readNextLine().ToCharArray()[0] == ampersand)
@@ -145,7 +132,7 @@ public class Dialogue : MonoBehaviour
             {
                 index++; // go to current choice line
                 currentLine(":", 1);
-                hideChoices();
+                DialogueManager.Instance.HideChoices();
                 while (readNextLine().ToCharArray()[0] == ampersand) // point to the place after the choice
                 {
                     index++;
@@ -186,7 +173,7 @@ public class Dialogue : MonoBehaviour
         foreach (char c in line)
         {
             dialogueText.text += c;
-            yield return new WaitForSeconds(speed);
+            yield return new WaitForSeconds(DialogueManager.Instance.waitTimeBetweenLetters);
         }
     }
 
@@ -229,187 +216,4 @@ public class Dialogue : MonoBehaviour
         isPlayerInRange = false;
         index = 0;
     }
-
-
-
-
-
-    /*[SerializeField] private TextAsset inkJSONQuest;
-    [SerializeField] private TextAsset inkJSONRegular;
-    [SerializeField] private float speed;
-
-    private TextMeshProUGUI text;
-    private GameObject dialogueBox;
-
-    private bool isPlayerInside;
-    private bool isPrintingQuestLines;
-
-    private Story regularStory;
-    private Story questStory;
-
-    private IQuestElement questElement;
-
-    private void Start()
-    {
-        text = Globals.Instance.DialogueText.GetComponent<TextMeshProUGUI>();
-        dialogueBox = Globals.Instance.DialogueBox;
-        text.text = string.Empty;
-        dialogueBox.SetActive(false);
-
-        print(inkJSONRegular.text);
-        regularStory = new Story(inkJSONRegular.text);
-        questStory = new Story(inkJSONQuest.text);
-
-        InitializeChoiceButtons(questStory); //choices are only during quest dialogues
-
-        questElement = GetComponent<IQuestElement>();
-    }
-
-    private void Update()
-    {
-        if (isPlayerInside)
-        {
-            if (isPrintingQuestLines) printLines(questStory);
-            else printLines(regularStory);
-        }
-    }
-
-    private void printLines(Story story)
-    {
-        if (InputManager.Instance.GetInteractionPressed())
-        {
-            if (text.text == story.currentText)
-            {
-                nextLine(story);
-            }
-            else
-            {
-                StopAllCoroutines();
-                text.text = story.currentText;
-            }
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player") && InputManager.Instance.GetInteractionPressed() && !isPlayerInside) // isPlayerInside check so it does not call startDialogue() multiple times while inside
-        {
-            isPlayerInside = true;
-            startDialogue();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isPlayerInside = false;
-            endDialogue();
-        }
-    }
-
-    private void startDialogue()
-    {
-        text.text = string.Empty;
-        if (questElement.StageBegin())
-        {
-            questStory.ResetState();
-            dialogueBox.SetActive(true);
-            isPrintingQuestLines = true;
-            nextLine(questStory);
-        }
-        else
-        {
-            regularStory.ResetState();
-            dialogueBox.SetActive(true);
-            isPrintingQuestLines = false;
-            nextLine(regularStory);
-        }
-    }
-
-    private void nextLine(Story story)
-    {
-        if (story.canContinue)
-        {
-            text.text = string.Empty;
-            StartCoroutine(typeLine(story));
-
-            if (story.currentChoices.Count != 0)
-            {
-                displayChoices(story);
-            }
-            else
-            {
-                hideChoices();
-            }
-        }
-        else 
-        {
-            endStage();
-        }
-    }
-
-    private void displayChoices(Story story)
-    {
-        int index = 0;
-        foreach (Choice choice in story.currentChoices)
-        {
-            Globals.Instance.Choices[index].SetActive(true);
-            Globals.Instance.Choices[index].GetComponentInChildren<TextMeshProUGUI>().text = choice.text;
-            index++;
-        }
-    }
-
-    private void InitializeChoiceButtons(Story story)
-    {
-        Globals.Instance.Choices[0].GetComponent<Button>().onClick.AddListener(() => MakeChoice(story, 0));
-        Globals.Instance.Choices[1].GetComponent<Button>().onClick.AddListener(() => MakeChoice(story, 1));
-        Globals.Instance.Choices[2].GetComponent<Button>().onClick.AddListener(() => MakeChoice(story, 2));
-    }
-
-    public void MakeChoice(Story story, int index)
-    {
-        if (index >= 0 && index < story.currentChoices.Count)
-        {
-            story.ChooseChoiceIndex(index);
-            nextLine(story);
-
-            if (index == 0) Scales.Instance.AddPleasure(1);
-            else if (index == 1) Scales.Instance.AddFear(1);
-            else if (index == 2) Scales.Instance.AddRealism(1);
-        }
-
-    }
-
-    private void hideChoices()
-    {
-        for (int i = 0; i < Globals.Instance.Choices.Length; i++)
-        {
-            GameObject choice = Globals.Instance.Choices[i];
-            choice.SetActive(false);
-        }
-    }
-
-    private void endStage()
-    {
-        if (isPrintingQuestLines) questElement.StageComplete();
-
-        endDialogue();
-    }
-
-    private void endDialogue()
-    {
-        StopAllCoroutines();
-        text.text = string.Empty;
-        dialogueBox.SetActive(false);
-    }
-
-    private IEnumerator typeLine(Story story)
-    {
-        foreach (char c in story.Continue().ToCharArray())
-        {
-            text.text += c;
-            yield return new WaitForSeconds(speed);
-        }
-    }*/
 }
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance { get; private set; }
 
+    public float waitTimeBetweenLetters;
+
     public GameObject DialogueBox;
     public GameObject DialogueText;
-    public GameObject[] Choices;
+    public GameObject ChoicePrefab;
+    public GameObject ChoicesParent;
+
+    private Button[] choices;
+    private Vector3 startPosForChoice = new Vector3(0, -75, 0);
 
     private void Awake()
     {
@@ -23,17 +30,41 @@ public class DialogueManager : MonoBehaviour
 
     private void Start()
     {
-        foreach (GameObject button in Choices)
-        {
-            button.SetActive(false);
-        }
+        ChoicesParent.SetActive(false);
         DialogueText.GetComponent<TextMeshProUGUI>().text = string.Empty;
         DialogueBox.SetActive(false);
+    }
+
+    public Button[] GetChoices()
+    {
+        return choices;
     }
 
     public void ShowBox()
     {
         DialogueBox.SetActive(true);
+    }
+
+    public Button[] ShowChoices(int quantity)
+    {
+        ChoicesParent.SetActive(true);
+        choices = new Button[quantity];
+        for (int i = 0; i < quantity; i++)
+        {
+            choices[i] = Instantiate(ChoicePrefab, ChoicesParent.transform).GetComponent<Button>();
+            choices[i].GetComponent<RectTransform>().localPosition =
+                new Vector3(startPosForChoice.x, startPosForChoice.y - 100 * i, startPosForChoice.z);
+        }
+
+        return choices;
+    }
+
+    public void HideChoices()
+    {
+        for (int i = 0; i < choices.Length; i++)
+        {
+            Destroy(choices[i].gameObject);
+        }
     }
 
     public void HideBox()
